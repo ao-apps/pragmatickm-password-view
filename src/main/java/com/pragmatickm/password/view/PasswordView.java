87c65e139418d431f853acd59dc4384ba7605497
AO Industries, Inc <support@aoindustries.com>
2021-03-11 08:44:03 -0600

    Created AnyContent interface and AnyDocument base implementation

--- src/main/java/com/pragmatickm/password/view/PasswordView.java
+++ src/main/java/com/pragmatickm/password/view/PasswordView.java
@@ -23,6 +23,7 @@
 package com.pragmatickm.password.view;
 
 import com.aoindustries.html.FlowContent;
+import com.aoindustries.html.servlet.DocumentEE;
 import com.aoindustries.servlet.http.Dispatcher;
 import com.pragmatickm.password.model.Password;
 import com.semanticcms.core.controller.PageUtils;
@@ -104,7 +105,7 @@ public class PasswordView extends View {
 	}
 
 	@Override
-	public <__ extends FlowContent<__>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException, SkipPageException {
+	public <__ extends FlowContent<DocumentEE, __>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException, SkipPageException {
 		Dispatcher.include(
 			servletContext,
 			JSPX_TARGET,
